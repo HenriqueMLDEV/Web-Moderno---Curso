3bfc69a24c3e583d25d94daa2d8e42dde3313e9d
HenriqueMLDEV <hmeloleal@gmail.com>
2024-10-24 22:28:25 -0300

    Atualizando - 24-10-2024

---
+++ 7-node/global.js
@@ -0,0 +1,5 @@
+console.log(global);
+
+global.MinhaApp = {
+    
+}
---
+++ 7-node/pastaA/pastaB/acessarModulos.js
@@ -0,0 +1,8 @@
+const moduloaA = require("../../moduloA");
+console.log(moduloaA.ola);
+
+const saudacao = require("saudacao");
+console.log(saudacao.ola);
+
+const c = require("./pastaC/index");
+console.log(c.ola2);
